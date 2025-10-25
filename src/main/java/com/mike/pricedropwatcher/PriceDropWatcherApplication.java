abc573a334f45a771b2accdb77635046855e081f
michalswig <mike@data.pl>
2025-10-25 11:28:51 +0200

    gitignore, help.md

--- src/main/java/com/mike/pricedropwatcher/PriceDropWatcherApplication.java
+++ src/main/java/com/mike/pricedropwatcher/PriceDropWatcherApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PriceDropWatcherApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PriceDropWatcherApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(PriceDropWatcherApplication.class, args);
+  }
 }
