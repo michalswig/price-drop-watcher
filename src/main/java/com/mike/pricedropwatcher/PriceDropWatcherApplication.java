21c5756999cbdd9006c2997536dcdc5134cdf0d1
michalswig <mike@data.pl>
2025-10-25 11:01:38 +0200

    project init

---
+++ src/main/java/com/mike/pricedropwatcher/PriceDropWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.mike.pricedropwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceDropWatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceDropWatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mike/pricedropwatcher/PriceDropWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.pricedropwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceDropWatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
